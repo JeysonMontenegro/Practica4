7db2e23b476b24ecab12f70b60e4b9304446bd45
JeysonMontenegro <jeysonmontenegro@gmail.com>
2015-04-25 09:40:22 -0600

    Actualizacion Pagina Consulta

--- app.js
+++ app.js
@@ -102,13 +102,13 @@ io.sockets.on('connection', function (socket) {
 socket.on('Query3', function (data1) {
 
 		var client = new pg.Client(conString);
-		var qu= 'select rd.NO_Tramo,d.Nombre,rd.Distancia_Origen,(sub2.Costo_Kilometro*rd.Distancia_Origen) from Ruta_destino rd,Destino d, (select ID_ruta from ticket where ID_Factura = 1 )as sub1,(Select Costo_Kilometro from bus where ID_bus=(select ID_bus  from Reservacion INNEr Join (select ID_Reservacion  from ticket where ID_Factura =  1 ) as sub1 ON Reservacion.ID_Reservacion=sub1.ID_reservacion))as sub2 WHERE sub1.ID_Ruta=rd.ID_Ruta and d.ID_Destino=rd.ID_DEstino;'
+		var qu= 'select rd.NO_Tramo,d.Nombre,rd.Distancia_Origen,(sub2.Costo_Kilometro*rd.Distancia_Origen)as costo from Ruta_destino rd , Destino d, (select ID_ruta from ticket where ID_Factura = $1 ) as sub1,(Select Costo_Kilometro from bus where ID_bus=(select ID_bus  from Reservacion INNEr Join (select ID_Reservacion  from ticket where ID_Factura =  $2 ) as sub1 ON Reservacion.ID_Reservacion=sub1.ID_reservacion))as sub2 WHERE sub1.ID_Ruta=rd.ID_Ruta and d.ID_Destino=rd.ID_DEstino';
 			
 		client.connect(function(err) {
   		if(err) {
    		 return console.error('could not connect to postgres', err);
  		 }
-  		client.query(data1.msg, function(err, result) {
+  		client.query(qu,[data1.msg, data1.msg], function(err, result) {
   		  if(err) {
     		  return console.error('error running query', err);
    			 }
